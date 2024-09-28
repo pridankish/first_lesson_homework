fed3123a4aa21edd185bfba6b281ba585541f195
Andrew Pridankin <awesome.pridankin@yandex.ru>
2024-09-28 09:54:43 +0700

    Initial Commit

---
+++ first_lesson_homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first_lesson_homework
+//
+//  Created by Андрей Приданкин on 28.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
